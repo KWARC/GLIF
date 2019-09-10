b2964320f6dc5d10035b916cecddb055f72a9b6a
Kai Amann <kai-amann@web.de>
2019-09-10 18:49:48 +0200

    deleted gf_kernel

--- gf_kernel/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A Test kernel"""
-
-__version__ = '1.0'
-
-from .kernel import GFKernel
--- gf_kernel/__main__.py
+++
@@ -1,4 +0,0 @@
-from ipykernel.kernelapp import IPKernelApp
-from . import GFKernel
-
-IPKernelApp.launch_instance(kernel_class=GFKernel)
